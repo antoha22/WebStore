6899ae7aec391cc956f353e1d6678f9a974d7c21
antoha22 <42731581+antoha22@users.noreply.github.com>
2020-03-06 06:40:22 +0300

    testHtmlPage

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("home controller, action - index");
+        }
+
+        public IActionResult SomeAction() 
+        {
+            return Content("action");
+        }
+    }
+}
