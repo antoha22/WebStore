8246ba84bdf1dc659a25a35bacc9941e09d9d96d
antoha22 <42731581+antoha22@users.noreply.github.com>
2020-03-06 07:00:13 +0300

    добавил представление сотрудников и представление по каждому сотруднику

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -8,14 +8,52 @@ namespace WebStore.Controllers
 {
     public class HomeController : Controller
     {
+        private static List<Employee> __Employees = new List<Employee>
+        {
+            new Employee
+            {
+                Id = 0,
+                FirstName = "Anton",
+                SurName = "Lvov",
+                Patronomyc = "Johnovich",
+                Age = 22
+            },
+            new Employee
+            {
+                Id = 0,
+                FirstName = "sad",
+                SurName = "Lv123123ov",
+                Patronomyc = "Johnofdsfsvich",
+                Age = 23
+            },
+            new Employee
+            {
+                Id = 0,
+                FirstName = "FFFF",
+                SurName = "SSSS",
+                Patronomyc = "AAAAA",
+                Age = 25
+            }
+        };
         public IActionResult Index()
         {
-            return Content("home controller, action - index");
+            return View();
         }
 
         public IActionResult SomeAction() 
         {
-            return Content("action");
+            return View();
+        }
+        public IActionResult Employees() 
+        {
+            return View(__Employees);
+        }
+        public IActionResult Employee(int Id) 
+        {
+            var employee = __Employees.FirstOrDefault(e => e.Id == Id);
+            if (employee is null)
+                return NotFound();
+            return View(employee);
         }
     }
 }
---
+++ Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebStore
+{
+    public class Employee
+    {
+        public int Id{ get; set; }
+        public string FirstName{ get; set; }
+        public string SurName{ get; set; }
+        public string Patronomyc{ get; set; }
+        public int Age { get; set; }
+    }
+}
